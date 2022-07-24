c6bd5908dc9594319a597f6be58a15adef02751c
Nickeytheblade <93241171+Nickeytheblade@users.noreply.github.com>
2022-07-23 20:23:51 -0700

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,12 @@
+secrets = {
+ 'ssid' : '####',
+ 'password' : '#####',
+ 'coincap_api_key' : '######',
+ 
+ 'coin1' : 'bitcoin',
+ 'coin2' : 'ethereum',
+ 'coin3' : 'helium',
+ 'coin1label' : 'BTC',
+ 'coin2label' : 'ETH',
+ 'coin3label' : 'HNT',
+ }
